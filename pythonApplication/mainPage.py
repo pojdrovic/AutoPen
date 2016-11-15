e11a17a6a5e8f8b03701e54799fa81fa98a1f40f
pojdrovic <Petar-PC@dhcp-wifi-8021x-155-41-24-113.bu.edu>
2016-11-15 15:37:39 -0500

    Added isntall for one more application

--- pythonApplication/mainPage.py
+++ pythonApplication/mainPage.py
@@ -3,81 +3,7 @@ from tkinter import Tk, BOTH, Label
 from tkinter.ttk import Frame, Button, Style
 #from PIL import Image, ImageTk
 
-
-class Example(Frame):
-
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-
-        self.parent = parent
-
-
-        self.parent.title("Quit button")
-        self.style = Style()
-        self.style.theme_use("default")
-
-        self.pack(fill=BOTH, expand=1)
-
-
-        w = 1000
-        h = 625
-
-        sw = self.parent.winfo_screenwidth()
-        sh = self.parent.winfo_screenheight()
-
-        x = (sw - w)/2
-        y = (sh - h)/2
-        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-        quitButton = Button(self, text="Quit",
-            command=self.quit)
-        quitButton.place(x=900, y=550)
-
-
-        def application1Click(self):
-            print("Hello")
-
-
-        def application2Click(self):
-            print("Hello, Again!")
-
-        def application3Click(self):
-            print("Hello, for the third time!")
-
-
-        app1 = Button(self, text="Application 1", command=self.application1Click)
-        app2 = Button(self, command=self.application2Click, text="Application 2")
-        app3 = Button(self, command=self.application3Click, text="Application 3")
-        app1.place(x=150, y=200)
-        app2.place(x=300, y=200)
-        app3.place(x=450, y=200)
-
-
-
-
-def main():
-
-    root = Tk()
-    root.geometry("250x150+300+300")
-    app = Example(root)
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
-
-
-            w = 1000
-        h = 625
-
-        sw = self.parent.winfo_screenwidth()
-        sh = self.parent.winfo_screenheight()
-
-        x = (sw - w)/2
-        y = (sh - h)/2
-        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-        '''
+''''
 
 
 from tkinter import Tk, Frame, Checkbutton, Button
@@ -133,21 +59,36 @@ class Example(Frame):
     def app1Click(self):
         print("application1")
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        dir_str = str(dir_path)
-        print(os.path.isdir(dir_str + "/Kayak"))
+        dir_str_os = str(dir_path)
+        dir_str = dir_str_os + "/Kayak"
+        print(os.path.isdir(dir_str))
         print(dir_path)
 
-        if(os.path.isdir(dir_str + "/Kayak") == False):
+        if(os.path.isdir(dir_str) == False):
             print("falsaasdfe")
             git.Git().clone("git://github.com/dschanoeh/Kayak")
             print("successfully cloned!")
 
-        if(os.path.isdir(dir_str + "/Kayak") == True):
+        if(os.path.isdir(dir_str) == True):
             print("We already have this repository!")
 
 
     def app2Click(self):
         print("application2")
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        dir_str_os = str(dir_path)
+        dir_str = dir_str_os + "/caringcaribou"
+        print(os.path.isdir(dir_str))
+
+        if(os.path.isdir(dir_str) == False):
+            print("Need to install the application")
+            git.Git().clone("https://github.com/CaringCaribou/caringcaribou.git")
+            print("Successfully cloned!")
+        
+        if(os.path.isdir(dir_str) == False):
+            print("We already have this repository")
+
+
 
     def app3Click(self):
         print("application3")
