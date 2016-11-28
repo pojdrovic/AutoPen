49200197ab0b3e360fcf5065da97b3abe5f52353
Dennis Your <lavashell@gmail.com>
2016-11-28 18:27:12 -0500

    Python Analysis Code
    for the first deliverable, reads in csv

---
+++ First Deliverable/import.py
@@ -0,0 +1,86 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from operator import itemgetter
+
+#class CANdata:
+#	def __init__(self):
+#		self.list = []
+#	def addrow(self,row):
+#		self.list.append(row)
+#		return
+
+
+def parsedata(data):
+	with open('Bus Traffic-1minuteBootUpEngineOff 1-27-2016 1-24-17 pm.csv', newline='') as csvfile:
+		canreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+		for ii in range (1,38):
+			next(canreader)
+		for row in canreader:
+			data.append(row)
+	return
+
+def finduniques(list):
+	templist = []
+	for x in list:
+		templist.append(x[9])
+	dataset = set(templist)
+	return dataset
+
+def printArbId(list):
+	for x in list:
+		print ('{:>12}  {:>12}'.format(x[0][9], len(x)))
+
+def plotArbId(starttime,endtime,list):
+	plt.ylabel('number of calls')
+	plt.xlabel('time (s)')
+	freqlist = [0] * (int((float(endtime)-float(starttime)))+1)
+	for x in list:
+		timefloor = math.floor(float(x[1])-float(starttime))
+		freqlist[timefloor] += 1
+	plt.plot(freqlist, 'ro')
+	plt.show()
+
+def main():
+#	data = CANdata();+
+	data = []
+	parsedata(data);
+	# print(sorted(data, key=itemgetter(9)))
+	uniquedata = []
+	dataset = finduniques(data)
+	for x in dataset:
+		templist = [row for row in data if row[9] == x]
+		uniquedata.append(templist)
+	##########uniquedata is where the data by ARB ID is stored
+	# for x in uniquedata[3]:
+	# 	print (x)
+	while (True):
+		print ('\n(1) list all ARB IDs')
+		print ('(2) print ARB ID calls over time')
+		print ('(3) write data to file')
+		command = input('Input command: ')
+		if (command == '1'):
+			printArbId(uniquedata);
+		elif (command == '2'):
+			arbID = input('ARB ID to search for: ')
+			for row in uniquedata:
+				if row[0][9] == arbID:
+					plotArbId(data[0][1],data[len(data)-1][1], row)
+					break;
+		elif (command == '3'):
+			writeto = input ('Enter file to write to: ')
+			print ('Writing to ' + writeto + '.csv...')
+			with open(writeto + '.csv', 'w') as csvfile:
+				csvwrite = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+				for iid in uniquedata:
+					for row in iid:
+						csvwrite.writerow(row)
+			print ('Write complete.')
+
+
+
+
+
+if __name__ == "__main__":
+	main()
