c292bd2aa052891c6c71765da10dcf51baa10446
velvetbleurose <jguerero@bu.edu>
2017-03-18 01:50:31 -0400

    replaced redundant code with functions

--- Second Deliverable/katoolin_installation.py
+++ Second Deliverable/katoolin_installation.py
@@ -17,35 +17,28 @@ Note: All information will be logged to /var/log/katoolin_installation.py
 
 import subprocess
 
-print ("Installing git...")
-git_EC = subprocess.run(["sudo", "apt-get", "install", "git"])
-if git_EC.returncode != 0:
-	print ("INSTALLATION FAILED: Could not install git")
-	print ("ERROR CODE:", git_EC.returncode)
-elif git_EC.returncode == 0:
-	print ("INSTALLATION SUCCESSFUL: Git successfully installed")
-	print ("Cloning Katoolin...")
-	katoolin_EC = subprocess.run(["git", "clone", "https://github.com/LionSec/katoolin.git"])
-	if katoolin_EC.returncode != 0:
-		print ("INSTALLATION FAILED: Could not clone Katoolin Repository and install Katoolin")
-		print ("ERROR CODE:", katoolin_EC.returncode)
-	elif katoolin_EC.returncode == 0:
-		print ("INSTALLATION SUCCESSFUL: Katoolin successfully installed")
-		print ("Setting /usr/bin/katoolin to executable...")
-		changemode_EC = subprocess.run(["chmod", "754", "/usr/bin/katoolin"]) #executable script for both you and your group but not for the world. 
-		if changemode_EC.returncode != 0:
-			print ("CONVERSION FAILED: Could not make /usr/bin/katoolin executable")
-			print ("ERROR CODE:", changemode_EC.returncode)
-		elif changemode_EC.returncode == 0:
-			print ("CONVERSION SUCCESSFUL: /usr/bin/katoolin set to executable")
-			enter_katoolin_EC = subprocess.run(["katoolin"])
-			print ("Starting up Katoolin...")
-			if enter_katoolin_EC.returncode != 0:
-				print ("KATOOLIN STARTUP FAILED: attempt to reinstall with the tool or go to README for instructions on how to install")
-				print ("ERROR CODE:", enter_katoolin_EC.returncode)
-			elif enter_katoolin_EC.returncode == 0:
-				print ("STARTUP SUCCESSFUL: Entering Katoolin...")
-			
+print ("Cloning Katoolin...")
+katoolin_EC = subprocess.run(["git", "clone", "https://github.com/LionSec/katoolin.git"])
+if katoolin_EC.returncode != 0:
+	print ("INSTALLATION FAILED: Could not clone Katoolin Repository and install Katoolin")
+	print ("ERROR CODE:", katoolin_EC.returncode)
+elif katoolin_EC.returncode == 0:
+	print ("INSTALLATION SUCCESSFUL: Katoolin successfully installed")
+	print ("Setting /usr/bin/katoolin to executable...")
+	changemode_EC = subprocess.run(["chmod", "754", "/usr/bin/katoolin"]) #executable script for both you and your group but not for the world. 
+	if changemode_EC.returncode != 0:
+		print ("CONVERSION FAILED: Could not make /usr/bin/katoolin executable")
+		print ("ERROR CODE:", changemode_EC.returncode)
+	elif changemode_EC.returncode == 0:
+		print ("CONVERSION SUCCESSFUL: /usr/bin/katoolin set to executable")
+		enter_katoolin_EC = subprocess.run(["katoolin"])
+		print ("Starting up Katoolin...")
+		if enter_katoolin_EC.returncode != 0:
+			print ("KATOOLIN STARTUP FAILED: attempt to reinstall with the tool or go to README for instructions on how to install")
+			print ("ERROR CODE:", enter_katoolin_EC.returncode)
+		elif enter_katoolin_EC.returncode == 0:
+			print ("STARTUP SUCCESSFUL: Entering Katoolin...")
+		
 
 '''
 
